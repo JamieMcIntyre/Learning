c43eda603bc882c61c7960e95ddec67145ae35f5
Jamie McIntyre <jamie-mcintyre@hotmail.com>
2012-06-02 15:46:41 -0700

    added root

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -44,11 +44,11 @@ class TopicsController < ApplicationController
 
     respond_to do |format|
       if @topic.save
-        format.html { redirect_to @topic, notice: 'Topic was successfully created.' }
-        format.json { render json: @topic, status: :created, location: @topic }
+        format.html { redirect_to topics_path, :notice => 'Topic was successfully created.' }
+        format.json { render :json => @topic, :status => :created, location => @topic }
       else
-        format.html { render action: "new" }
-        format.json { render json: @topic.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render :json => @topic.errors, :status => :unprocessable_entity }
       end
     end
   end
